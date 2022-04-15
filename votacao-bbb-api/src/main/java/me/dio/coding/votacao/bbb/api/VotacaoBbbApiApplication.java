64fd98ec3e7b455815d48ba9b0fe8d6553f98aa5
cunharodrigues <pwcunha@gmail.com>
2022-04-15 16:51:36 -0300

    Full commit

--- votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/VotacaoBbbApiApplication.java
+++ votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/VotacaoBbbApiApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+/*@ComponentScan(basePackages={"me.dio.coding.votacao.bbb.api.repository"})*/
 public class VotacaoBbbApiApplication {
 
 	public static void main(String[] args) {
--- votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/controller/ParametrizationController.java
+++ votacao-bbb-api/src/main/java/me/dio/coding/votacao/bbb/api/controller/ParametrizationController.java
@@ -1,13 +1,12 @@
 package me.dio.coding.votacao.bbb.api.controller;
 
-
 import java.util.Optional;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import me.dio.coding.votacao.bbb.api.model.ParametrizationModel;
@@ -16,33 +15,33 @@ import me.dio.coding.votacao.bbb.api.repository.ParametrizationRepository;
 @RestController
 @RequestMapping("/api/parametros")
 public class ParametrizationController {
+	
+  private final ParametrizationRepository repository;
 
-	
-	private final ParametrizationRepository repository;
+  public ParametrizationController(ParametrizationRepository repository) {
+    this.repository = repository;
+  }
 
-	public ParametrizationController(ParametrizationRepository repository) {
-		this.repository= repository;
-		
-	}
-	
-	public ParametrizationRepository getRepository() {
-		return repository;
-	}
-		
-	@PostMapping("/salvar")
-	public ResponseEntity<ParametrizationModel> salvar(@RequestBody ParametrizationModel parametro) {
-		ParametrizationModel entity = repository.save(parametro);
-		return ResponseEntity.ok(entity);
-		
-	}
-	
-	@GetMapping("/consultar")
-	public ResponseEntity<ParametrizationModel> consultar(@RequestBody String chave) {
-		Optional<ParametrizationModel> optParametro = repository.findById(chave);
-		if (optParametro.isPresent()) {
-			return ResponseEntity.notFound().build();
-		}
-		return ResponseEntity.ok(optParametro.get());
-		
-	}
+  public ParametrizationRepository getRepository() {
+    return repository;
+  }
+
+  @PostMapping("/salvar")
+  public ResponseEntity<ParametrizationModel> salvar(
+    @RequestBody ParametrizationModel parametro
+  ) {
+    ParametrizationModel entidade = repository.save(parametro);
+    return ResponseEntity.ok(entidade);
+  }
+
+  @GetMapping("/consultar")
+  public ResponseEntity<ParametrizationModel> consultar(
+    @RequestParam String chave
+  ) {
+    Optional<ParametrizationModel> optParametro = repository.findById(chave);
+    if (optParametro.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+    return ResponseEntity.ok(optParametro.get());
+  }
 }
