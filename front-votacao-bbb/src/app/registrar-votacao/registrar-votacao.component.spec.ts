0e995583459ee3467bcb183766f71cc111d05b9f
cunharodrigues <pwcunha@gmail.com>
2022-04-24 17:28:12 -0300

    Project ended

---
+++ front-votacao-bbb/src/app/registrar-votacao/registrar-votacao.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RegistrarVotacaoComponent } from './registrar-votacao.component';
+
+describe('RegistrarVotacaoComponent', () => {
+  let component: RegistrarVotacaoComponent;
+  let fixture: ComponentFixture<RegistrarVotacaoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ RegistrarVotacaoComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(RegistrarVotacaoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
